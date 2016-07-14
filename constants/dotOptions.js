ac19b0f52f7db7e82c2f9fe40adf3f1113bd259b
dawnington <dawnington@gmail.com>
2016-07-14 16:59:45 -0700

    added mroe sounds and dots

--- constants/dotOptions.js
+++ constants/dotOptions.js
@@ -1,11 +1,114 @@
 module.exports = {
-  a: {
+  // big
+  z: {
+    color: '#baecf0',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  x: {
     color: '#ed6e2f',
     radius: [175, 200],
     endRadius: 200,
     duration: [500, 1000],
     numDots: 1,
   },
+  c: {
+    color: '#f6c7df',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  v: {
+    color: '#f4de70',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  b: {
+    color: '#fbfbf4',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  n: {
+    color: '#222121',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  m: {
+    color: '#222121',
+    radius: [175, 200],
+    endRadius: 200,
+    duration: [500, 1000],
+    numDots: 1,
+  },
+  // mid
+  f: {
+    color: '#f4de70',
+    radius: [25, 50],
+    duration: [500, 1000],
+    endRadius: 35,
+    numDots: 10,
+  },
+  // medium large
+  a: {
+    color: '#baecf0',
+    radius: [50, 100],
+    duration: [500, 1000],
+    endRadius: 100,
+    numDots: 10,
+  },
+  s: {
+    color: '#f6c7df',
+    radius: [50, 100],
+    duration: [500, 1000],
+    endRadius: 100,
+    numDots: 10,
+  },
+  d: {
+    color: '#ed6e2f',
+    radius: [50, 100],
+    duration: [500, 1000],
+    endRadius: 100,
+    numDots: 10,
+  },
+  // small
+  h: {
+    color: '#fbfbf4',
+    radius: [15, 25],
+    duration: [1000, 2000],
+    endRadius: 0,
+    numDots: 25,
+  },
+  j: {
+    color: '#222121',
+    radius: [10, 15],
+    duration: [1000, 2000],
+    endRadius: 0,
+    numDots: 30,
+  },
+  k: {
+    color: '#f6c7df',
+    radius: [10, 15],
+    duration: [500, 1000],
+    endRadius: 0,
+    numDots: 30,
+  },
+  l: {
+    color: '#f4de70',
+    radius: [10, 15],
+    duration: [500, 1000],
+    endRadius: 0,
+    numDots: 30,
+  },
+  // TBD
   w: {
     color: '#ed6e2f',
     radius: [125, 150],
@@ -13,39 +116,11 @@ module.exports = {
     endRadius: 0,
     numDots: 5,
   },
-  s: {
+  e: {
     color: '#f6c7df',
     radius: [100, 120],
     duration: [6000, 8000],
     endRadius: 0,
     numDots: 5,
-  },
-  d: {
-    color: '#baecf0',
-    radius: [50, 100],
-    duration: [4000, 6000],
-    endRadius: 0,
-    numDots: 10,
-  },
-  f: {
-    color: '#f4de70',
-    radius: [25, 50],
-    duration: [3000, 4000],
-    endRadius: 0,
-    numDots: 20,
-  },
-  g: {
-    color: '#fbfbf4',
-    radius: [15, 25],
-    duration: [2000, 3000],
-    endRadius: 0,
-    numDots: 25,
-  },
-  h: {
-    color: '#222121',
-    radius: [10, 15],
-    duration: [1000, 2000],
-    endRadius: 0,
-    numDots: 30,
   },
 };
